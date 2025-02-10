7180929e4fdcac6779a2277b74e159aef338fc2a
Yoana Bastiyanova <bastianovajoana@gmail.com>
2025-02-11 01:53:36 +0200

    completed trains.py

--- fundamentals/List Advanced - Lab/trains.py
+++ fundamentals/List Advanced - Lab/trains.py
@@ -1,3 +1,16 @@
 train_nums = int(input())
 wagons = [0] * train_nums
+
+command = input().split(' ')
+while command[0] != 'End':
+
+    if command[0] == 'add':
+        wagons[-1] += int(command[1])
+    elif command[0] == 'insert':
+        wagons[int(command[1])] += int(command[2])
+    elif command[0] == 'leave':
+        wagons[int(command[1])] -= int(command[2])
+
+    command = input().split(' ')
+
 print(wagons)
