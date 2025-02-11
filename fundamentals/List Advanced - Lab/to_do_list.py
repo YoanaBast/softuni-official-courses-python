d6d1721f06ee84909786c9ef13056c6a9723d852
Yoana Bastiyanova <bastianovajoana@gmail.com>
2025-02-11 12:48:40 +0200

    completed to_do_list.py

---
+++ fundamentals/List Advanced - Lab/to_do_list.py
@@ -0,0 +1,14 @@
+to_do_list = []
+command = input()
+while command != 'End':
+    to_do_list.append(command)
+
+
+    command = input()
+
+sorted_list = sorted(to_do_list)
+final_list = []
+for item in sorted_list:
+    final_list.append(item[2::])
+
+print(final_list)
