1474d66e5eb99457c9feed8f2bcb03c06df491f5
Yoana Bastiyanova <bastianovajoana@gmail.com>
2025-03-29 16:49:36 +0200

    fixed bug

--- fundamentals/Exam Prep 2/world_tour.py
+++ fundamentals/Exam Prep 2/world_tour.py
@@ -16,14 +16,15 @@ while True:
     elif command[0] == 'Remove Stop':  # "Remove Stop:{start_index}:{end_index}"
     # Remove the elements of the string from the starting index to the end index (inclusive) if both indices are valid.
         start_index, end_index = int(command[1]), int(command[2])
-        if start_index < len(stops) and end_index <= len(stops):
+        if start_index < len(stops) and end_index < len(stops): # tuka issueto beshe <= vmesto <
             stops = stops[:start_index] + '' + stops[end_index+1:]
         print(stops)
 
     elif command[0] == 'Switch':  # "Switch:{old_string}:{new_string}"
     # If the old string is in the initial string, replace it with the new one (all occurrences)
         old_string, new_string = command[1], command[2]
-        while old_string in stops:
+        count = stops.count(old_string)
+        for _ in range(count):
             stops = stops.replace(old_string, new_string)
         print(stops)
 
