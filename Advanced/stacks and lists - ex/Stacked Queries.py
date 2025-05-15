cec3ee28601eb11f2537819124d4932b77563e23
Yoana Bastiyanova <bastianovajoana@gmail.com>
2025-05-15 14:29:58 +0300

    .

--- Advanced/stacks and lists - ex/Stacked Queries.py
+++ Advanced/stacks and lists - ex/Stacked Queries.py
@@ -1,17 +1,16 @@
 n = int(input())
 stack = []
+#mapper:
+functions = {
+    '1': lambda x: stack.append(int(x)),
+    '2': lambda: stack.pop() if stack else None,
+    '3': lambda: print(max(stack)) if stack else None,
+    '4': lambda: print(min(stack)) if stack else None,
+}
 
 for _ in range(n):
     command = input().split()
-    if command[0] == '1':
-        stack.append(int(command[1]))
-    elif stack:
-        if command[0] == '2':
-                stack.pop()
-        elif command[0] == '3':
-                print(max(stack))
-        elif command[0] == '4':
-                print(min(stack))
+    functions[command[0]](*command[1:])
 
 stack.reverse()
 print(', '.join(map(str, stack)))
