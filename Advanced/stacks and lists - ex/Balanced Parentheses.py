231b59d544ea62821f4bf61e36540f23f563680c
Yoana Bastiyanova <bastianovajoana@gmail.com>
2025-05-16 17:16:27 +0300

    .

---
+++ Advanced/stacks and lists - ex/Balanced Parentheses.py
@@ -0,0 +1,22 @@
+# {[()]} - Parentheses are balanced.
+# (){}[] - Parentheses are balanced.
+# {[(])} - Parentheses are NOT balanced.
+op_stack = []
+opening = ['{', '[', '(']
+closing = ['}', ']', ')']
+balanced = True
+
+par = list(input())
+for p in par:
+    if p in opening:
+        op_stack.append(p)
+    elif p in closing:
+        if not op_stack: # if closing is the first one = false
+            balanced = False
+            break
+        if closing.index(p) != opening.index(op_stack[-1]):
+            balanced = False
+            break
+        op_stack.pop()
+
+print('YES') if balanced else print('NO')
