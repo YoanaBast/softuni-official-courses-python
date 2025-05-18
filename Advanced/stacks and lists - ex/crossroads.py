e9a11082ab29a4b3f9ddbf14d20f3ecab38f3625
Yoana Bastiyanova <bastianovajoana@gmail.com>
2025-05-18 18:13:03 +0300

    .

--- Advanced/stacks and lists - ex/crossroads.py
+++ Advanced/stacks and lists - ex/crossroads.py
@@ -1,38 +1,45 @@
 from collections import deque
-green = int(input())
-free = int(input())
-safe  =  green + free
-cars = deque([])
-no_crash = True
-passed = 0
+
+green_light_dur = int(input())
+free_win_duration = int(input())
+
+queue = deque()
+
+total_passed = 0
+crash = False
 
 while True:
     command = input()
     if command == 'END':
         break
-    cars.append(command)
+    elif command == 'green':
+        time_left = green_light_dur
 
-time_left = safe
-while cars:
-    if cars[0] == 'green':
-        time_left = safe
-        cars.popleft()
-        continue
-    car_len = len(cars[0])
-    if time_left >= free: #car goes
-        if car_len <= time_left:
-            cars.popleft()
-            time_left -= car_len
-            passed += 1
-        else:
-            crash = cars[0][time_left]
-            no_crash = False
-            print('A crash happened!')
-            print(f'{cars[0]} was hit at {crash}.')
+        while queue and time_left > 0:
+            car = queue.popleft()
+            car_length = len(car)
+
+            if car_length <= time_left:
+                time_left -= car_length
+                total_passed += 1
+            else:
+                # Check if it can still pass during the free window
+                if car_length <= time_left + free_win_duration:
+                    total_passed += 1
+                    break  # green time and window consumed
+                else:
+                    # Crash occurs
+                    crash_index = time_left + free_win_duration
+                    crash_letter = car[crash_index]
+                    print("A crash happened!")
+                    print(f"{car} was hit at {crash_letter}.")
+                    crash = True
+                    break
+        if crash:
             break
     else:
-        cars.popleft()
+        queue.append(command)
 
-if no_crash:
-    print(f'Everyone is safe.\n{passed} total cars passed the crossroads.')
-
+if not crash:
+    print("Everyone is safe.")
+    print(f"{total_passed} total cars passed the crossroads.")
