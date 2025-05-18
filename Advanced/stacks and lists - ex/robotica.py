62036234dfa1057e89c79c1bf145d772cf9c56c2
Yoana Bastiyanova <bastianovajoana@gmail.com>
2025-05-18 17:03:15 +0300

    .

--- Advanced/stacks and lists - ex/robotica.py
+++ Advanced/stacks and lists - ex/robotica.py
@@ -1,15 +1,21 @@
 from collections import deque
 
+#ROBOTS
 _robots = input().split(';')
-robo_q = deque()
+robots_list = []
 for _robot in _robots:
     name, time = _robot.split('-')
-    robo_q.append({'name': name, 'time': int(time), 'av': 0})
+    robots_list.append({'name': name,
+                   'time': int(time),
+                   'av': 0})
+# no need for deque in the robots, will not rotate?
 
+#TIME
 H, M, S = map(int, input().split(':'))  #8:00:00
 time_seconds = H * 3600  + M * 60 + S
 
-items = deque([])
+#ITEMS
+items = deque()
 while True:
     command = input()
     if command == 'End':
@@ -17,24 +23,23 @@ while True:
     items.append(command)
 
 while items:
-    time_seconds += 1
+    curr_item = items.popleft() #moved to top
+    time_seconds += 1 #moved here
+    is_taken = False #NEW
 
-    N = robo_q[0]['name']
-    if robo_q[0]['av'] == 0 or robo_q[0]['av'] <= time_seconds:
-        curr_item = items.popleft()
-        robo_q[0]['av'] =  time_seconds
-        robo_q[0]['av'] += robo_q[0]['time']
-        adjusted_time = time_seconds % 86400
-        h = adjusted_time // 3600
-        m = (adjusted_time % 3600) // 60
-        s = adjusted_time % 60
-        curr_time = f"[{h:02d}:{m:02d}:{s:02d}]"
-        print(f'{N} - {curr_item} {curr_time}')
-        robo_q.rotate(-1)
-        continue
-    else:
-        robo_q.rotate(-1)
-        items.rotate(-1)
+    for robot in robots_list:
+        if robot ['av'] <= time_seconds: #NEW
+            robot['av'] = time_seconds + robot['time']
+            h = time_seconds // 3600
+            m = (time_seconds % 3600) // 60
+            s = (time_seconds % 60) % 60
+            h %= 24
+            curr_time = f"[{h:02d}:{m:02d}:{s:02d}]"
+            print(f'{robot["name"]} - {curr_item} {curr_time}')
+            is_taken = True
+            break
+    if not is_taken:
+        items.append(curr_item)
 
-        # 83/100
+            # 83/100
         # check https://pastebin.com/pd2guPu0
