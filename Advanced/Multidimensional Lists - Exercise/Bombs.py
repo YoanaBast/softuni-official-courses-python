e0494beb27fcc128c9715f3f5601881065a76e07
Yoana Bastiyanova <bastianovajoana@gmail.com>
2025-05-23 19:42:49 +0300

    .

--- Advanced/Multidimensional Lists - Exercise/Bombs.py
+++ Advanced/Multidimensional Lists - Exercise/Bombs.py
@@ -24,3 +24,35 @@ print(f"Sum: {sum(alive)}")
 
 for r in matrix:
     print(*r)
+
+"""
+rows = cols = int(input())
+matrix = [[int(x) for x in input().split(' ')] for _ in range(rows)]
+alive = []
+
+coordinates = input().split(' ')
+for coor in coordinates: #[1,2] [2,1] [2,0]
+    r, c = [int(x) for x in coor.split(',')] # r = 1, c = 2
+    if r < rows and c < cols:
+        bomb = matrix[r][c]
+
+    for row in range(rows): #0-3
+        for col in range(cols): #0-3
+
+            if [row, col] not in coordinates and matrix[row][col] > 0: #not the bomb
+
+                if row == r - 1 or row == r or row ==  r + 1:
+                    if col == c - 1 or col == c or col == c + 1:
+                        matrix[row][col] -= bomb
+
+for row in range(rows): #0-3
+    for col in range(cols): #0-3
+        if matrix[row][col] > 0:
+            alive.append(matrix[row][col])
+
+print(f"Alive cells: {len(alive)}")
+print(f"Sum: {sum(alive)}")
+
+for rrr in matrix:
+    print(*rrr)
+"""
