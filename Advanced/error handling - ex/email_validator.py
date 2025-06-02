45d6714140497dd33be1e3a63addec88dc5b7c5a
Yoana Bastiyanova <bastianovajoana@gmail.com>
2025-06-02 10:57:26 +0300

    .

---
+++ Advanced/error handling - ex/email_validator.py
@@ -0,0 +1,35 @@
+import regex as re
+
+
+class NameTooShortError(Exception):
+    pass
+
+class MustContainAtSymbolError(Exception):
+    pass
+
+class InvalidDomainError(Exception):
+    pass
+
+
+emails = []
+domains = ('.com', '.bg', '.org', '.net')
+name_re = r"[a-zA-Z]{4,}@"
+
+
+while True:
+    email = input()
+
+    if email == 'End':
+        break
+
+    if not re.match(name_re, email): #  match object / None
+        raise NameTooShortError("Name must be more than 4 characters")
+
+    if '@' not in email:
+        raise MustContainAtSymbolError("Email must contain @")
+
+    if not email.endswith(domains): #  .endswith() can take str or a tuple of str and will auto iterate
+        raise InvalidDomainError("Domain must be one of the following: .com, .bg, .org, .net")
+
+
+    print("Email is valid")
