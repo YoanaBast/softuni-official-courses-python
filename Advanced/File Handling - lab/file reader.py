bdac71c5c14d66de118857996c3938bd5f89d8b5
Yoana Bastiyanova <bastianovajoana@gmail.com>
2025-06-05 11:23:24 +0300

    .

--- Advanced/File Handling - lab/file reader.py
+++ Advanced/File Handling - lab/file reader.py
@@ -2,11 +2,12 @@ import os
 from constants import ABSOLUTE_PROJECT_PATH
 
 path = os.path.join(ABSOLUTE_PROJECT_PATH, 'file_folder', 'numbers.txt')
-file = open(path)
 
-numbers = [int(x) for x in file.read().split('\n')]
-print(sum(numbers))
+with open(path) as file: # with manager - file is open only for the indented block and will close as soon as the block ends
+    numbers = [int(x) for x in file.read().split('\n')]
+    print(sum(numbers))
 
+print(file.closed)
 
 
 
--- Advanced/File Handling - lab/filr_opener.py
+++ Advanced/File Handling - lab/filr_opener.py
@@ -1,12 +1,12 @@
 import os
 
-path  = os.path.join("..", "file_folder", "text.xt")
+path  = os.path.join("..", "file_folder", "text.txt")
 
 
 try:
-    open(path)
+    file = open(path)
     print('File Found')
+    file.close()
 except FileNotFoundError:
     print("File not found")
 
-close(path)
