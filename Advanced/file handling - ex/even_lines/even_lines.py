015ecffa22838c2d2e768a20435c94629a554d35
Yoana Bastiyanova <bastianovajoana@gmail.com>
2025-06-06 17:02:16 +0300

    .

---
+++ Advanced/file handling - ex/even_lines/even_lines.py
@@ -0,0 +1,20 @@
+
+text_path = 'text.txt'
+syms = {"-", ",", ".", "!", "?"}
+
+lines = []
+with open(text_path) as f:
+    for index, line in enumerate(f):
+        if index % 2 == 0:
+            line_lst = line.split()
+
+            for inx, word in enumerate(line_lst):
+                for char in word:
+                    if char in syms:
+                        word = word.replace(char, '@')
+                        line_lst[inx] = word
+
+            lines.append(list(reversed(line_lst)))
+
+for item in lines:
+    print(' '.join(item))
