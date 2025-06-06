273bac64035ffdfaa1a815619f392d8a4a935df3
Yoana Bastiyanova <bastianovajoana@gmail.com>
2025-06-06 17:33:56 +0300

    .

---
+++ Advanced/file handling - ex/line_numbers/line_numbers.py
@@ -0,0 +1,18 @@
+text_path = 'text.txt'
+output = 'output.txt'
+
+syms = {"-", ",", ".", "!", "?", "'"}
+lines = []
+
+with open(output, 'a') as o:
+    with open(text_path) as f:
+        for i, line in enumerate(f):
+            line = line.strip()
+            no_spaces = line.replace(" ", "")
+            s = 0
+            for x in syms:
+                s += line.count(x)
+            o.write(f'Line {i}: {line} ({(len(no_spaces) - s)})({s})\n')
+
+# make sure the output file is empty before running
+
